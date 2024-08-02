3c09b78ecca148d69db6afa3417bfadfcbf15bd9
César <cesarlpb@gmail.com>
2024-08-02 15:58:31 +0200

    update en CREATE TABLE para SQLite

--- SQL/0.crear_tabla.sql
+++ SQL/0.crear_tabla.sql
@@ -1,14 +1,15 @@
--- SQL para crear la tabla de los ejercicios:
+-- SQL para crear la tabla de los ejercicio
+-- versión modificada para SQLITE3:
 CREATE TABLE IF NOT EXISTS tblUsuarios (
-   idx INT PRIMARY KEY AUTO_INCREMENT,
-   usuario VARCHAR(20),
-   nombre VARCHAR(20),
-   sexo VARCHAR(1),
-   nivel TINYINT,
-   email VARCHAR(50),
-   telefono VARCHAR(20),
-   marca VARCHAR(20),
-   compañia VARCHAR(20),
-   saldo FLOAT,
-   activo BOOLEAN
+   idx INTEGER PRIMARY KEY AUTOINCREMENT,
+   usuario TEXT NOT NULL,
+   nombre TEXT NOT NULL,
+   sexo TEXT CHECK(sexo IN ('M', 'F')),
+   nivel INTEGER CHECK(nivel BETWEEN 0 AND 255),
+   email TEXT,
+   telefono TEXT,
+   marca TEXT,
+   compañia TEXT,
+   saldo REAL,
+   activo INTEGER CHECK(activo IN (0, 1))
 );
